21e889326e258ec4b4d68a3cfe217d22a0462453
Mykhailo Khanukaiev <mykhailokhanukaiev@Mykhailos-MacBook-Air.local>
2021-05-16 18:57:07 +0300

    profile corrected

--- client/components/profile.js
+++ client/components/profile.js
@@ -10,7 +10,7 @@ const Profile = () => {
       <div className="flex items-center justify-center h-screen">
         <div className="bg-indigo-800 text-white font-bold rounded-lg border shadow-lg p-10">
           <div id="title"> Profile </div>
-          <div id="user"> { user } </div>
+          <div id="username"> { user } </div>
           <div>
               <Link to="/dashboard/main"> Go to Main </Link>
           </div>
